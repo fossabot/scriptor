c6f88f213a15e685ff865f5a26b2f74a920e4569
Adam Garza <adam.garza@comfora.org>
2025-12-04 16:19:20 +0000

    Add pre-commit configuration, logging, and update dependencies
    - Introduced a new pre-commit configuration file to enforce code quality checks using ruff and other hooks.
    - Added loguru for enhanced logging capabilities in the application.
    - Updated `pyproject.toml` to include loguru and pre-commit as dependencies.
    - Enhanced logging in the main application to provide debug information during execution.
    - Refactored the initialization command to log warnings and info messages.
    - Created a new empty `project.py` file for future development.
    - Updated `uv.lock` with new package dependencies and versions.

--- src/scriptor/__init__.py
+++ src/scriptor/__init__.py
@@ -1,6 +1,13 @@
 """
 Scriptor is a open-source CLI tool designed for simple management of your local and cloud environments tailored to your home-labs or business environments
 """
-from importlib.metadata import version
 
-__VERSION__ = version('scriptor')
+import sys
+from importlib.metadata import version
+from loguru import logger
+
+__VERSION__ = version("scriptor")
+
+logger.remove()
+logger.add(sys.stderr, level="WARNING")
+logger.add("scriptor.log", rotation="50 MB", level="DEBUG")
--- src/scriptor/main.py
+++ src/scriptor/main.py
@@ -4,15 +4,18 @@ import os
 from rich import print
 from rich.panel import Panel
 from typing_extensions import Annotated
-from scriptor import __VERSION__
+from scriptor import __VERSION__, logger
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
+
 
 @app.callback()
 def callback():
     """
     Scriptor is a open-source CLI tool designed for simple management of your local and cloud environments tailored to your home-labs or business environments
     """
+    logger.debug(f"Command invoked running {__VERSION__} version of Scriptor.")
+
 
 @app.command(name="version")
 def version():
@@ -21,8 +24,16 @@ def version():
     """
     print(Panel.fit(f"Installed Scriptor Version: [red]{__VERSION__}"))
 
+
 @app.command(name="init")
-def initialize(override: Annotated[bool, typer.Option("--override", help="Override existing configuration file if it exists")] = False):
+def initialize(
+    override: Annotated[
+        bool,
+        typer.Option(
+            "--override", help="Override existing configuration file if it exists"
+        ),
+    ] = False,
+):
     """
     Initializes a Scriptor project by setting up necessary configurations and directories.
     """
@@ -30,20 +41,29 @@ def initialize(override: Annotated[bool, typer.Option("--override", help="Overri
         "name": "example-scriptor-project",
         "version": "0.1.0",
         "description": "An example Scriptor project initialized using the CLI.",
-
         "endpoint": "main.py",
         "SCRIPTOR_API_VERSION": "v0",
     }
+    logger.debug(
+        f"Prepped API version {jsonContent['SCRIPTOR_API_VERSION']} for project initialization."
+    )
 
     if os.path.exists("scriptor.json") and not override:
-        print("Configuration file already exists, please add --override to recreate the file.")
+        logger.warning("Configuration already exists and --override not specified.")
+        print(
+            "Configuration file already exists, please add --override to recreate the file."
+        )
         return
-    with open("scriptor.json", 'w') as file:
+    with open("scriptor.json", "w") as file:
+        logger.info("Creating scriptor.json configuration file.")
         file.write(json.dumps(jsonContent, indent=4))
 
-    print("Scriptor compatible project created.\n" \
-    "Note that Scriptor is still [yellow]under development[/yellow], some features may not work as expected and major changes are expected.")
+    print(
+        "Scriptor compatible project created.\n"
+        "Note that Scriptor is still [yellow]under development[/yellow], some features may not work as expected and major changes are expected."
+    )
     return
+
 
 if __name__ == "__main__":
     app()
